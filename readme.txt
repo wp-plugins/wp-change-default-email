b1d77df48d6bc4e1e96fe04f997f7edf24bb3b7b
sharmavijay79 <sharmavijay79@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-07 04:21:19 +0000

    First Version of Plugin
    git-svn-id: https://plugins.svn.wordpress.org/wp-change-default-email/trunk@834211 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,35 @@
+=== WP Change Default Email ===
+Contributors: sharmavijay79
+Tags: wp mail, mail, email, change default email
+Donate link:
+https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=JKWDG57E8SCMA
+Requires at least: 3.3
+Tested up to: 3.8
+Stable tag: trunk
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Allows WordPress Admins to update the default WordPress From Email Address and
+Name. 
+
+== Description ==
+By default WordPress uses \'wordpress@your-domain.com\' as the email address
+and \'WordPress\' as From name. This plugin allows WordPress Admins to update
+the default WordPress From Email Address and Name. 
+
+
+== Installation ==
+1. Upload WP Change Default Email plugin to your blog, Activate it, then enter
+the desired name and email address.
+1, 2, 3: You\'re done!
+
+== Screenshots ==
+1. Go to Plugins Settings Screen and enter your Email Address and Name
+
+== Changelog ==
+= 0.1 =
+* Initial release.
+
+== Upgrade Notice ==
+= 0.1 =
+* Initial release.
---
+++ wp-change-default-email.php
@@ -0,0 +1,62 @@
+<?php
+/*
+Plugin Name: WP Change Default Email
+Plugin URI: http://www.techeach.com/wordpress-plugins/
+Description: WP Change Default Email allows you to change the default from email and name.
+Version: 0.1
+License: GPLv2
+Author: Vijay Sharma
+Author URI: http://www.techeach.com/
+*/
+
+
+$wcdeOptions = get_option('wp_change_default_email_options');
+
+function wp_change_default_email() {
+        add_filter('wp_mail_from', 'wp_change_default_email_change_from_email');
+	add_filter('wp_mail_from_name', 'wp_change_default_email_change_from_name');
+}
+add_filter('init','wp_change_default_email');
+
+function wp_change_default_email_change_from_email($email_old) { 
+	global $wcdeOptions;
+	if ( isset($wcdeOptions['from']) && is_email($wcdeOptions['from'])) {
+	    return $wcdeOptions['from'];	
+	} else {
+	    return $email_old;	
+	}
+}
+
+function wp_change_default_email_change_from_name($fromname) {
+        global $wcdeOptions;
+        if ( !empty($wcdeOptions['fromname'])) {
+	 	return $wcdeOptions['fromname'];
+	} else {
+		return $fromname;
+	}
+}
+
+function wp_change_default_email_activate(){
+	$wcdeOptions = array();
+	$wcdeOptions["from"] = "";
+	$wcdeOptions["fromname"] = "";
+	$wcdeOptions["deactivate"] = "";
+	add_option("wp_change_default_email_options",$wcdeOptions);
+}
+register_activation_hook( __FILE__ , 'wp_change_default_email_activate' );
+	
+if($wcdeOptions["deactivate"]=="yes"){
+	register_deactivation_hook( __FILE__ , create_function('','delete_option("wp_change_default_email_options");') );
+}
+
+function wp_change_default_email_settings_link($action_links,$plugin_file){
+	if($plugin_file==plugin_basename(__FILE__)){
+		$ws_settings_link = '<a href="options-general.php?page=' . dirname(plugin_basename(__FILE__)) . '/wp_change_default_email_admin.php">' . __("Settings") . '</a>';
+		array_unshift($action_links,$ws_settings_link);
+	}
+	return $action_links;
+}
+add_filter('plugin_action_links','wp_change_default_email_settings_link',10,2);
+
+//If this is admin load the settings page in Admin
+if(is_admin()) require_once('wp_change_default_email_admin.php');
