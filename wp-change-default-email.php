838bb17ad7ce08850ca6d24e7b535b3324969da8
sharmavijay79 <sharmavijay79@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-09 17:34:27 +0000

    Added support for languages.
    git-svn-id: https://plugins.svn.wordpress.org/wp-change-default-email/trunk@890218 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-change-default-email.php
+++ wp-change-default-email.php
@@ -3,7 +3,7 @@
 Plugin Name: WP Change Default Email
 Plugin URI: http://www.techeach.com/wordpress-plugins/
 Description: WP Change Default Email allows you to change the default from email and name.
-Version: 0.1
+Version: 0.2
 License: GPLv2
 Author: Vijay Sharma
 Author URI: http://www.techeach.com/
@@ -14,7 +14,7 @@ $wcdeOptions = get_option('wp_change_default_email_options');
 
 function wp_change_default_email() {
         add_filter('wp_mail_from', 'wp_change_default_email_change_from_email');
-	add_filter('wp_mail_from_name', 'wp_change_default_email_change_from_name');
+		add_filter('wp_mail_from_name', 'wp_change_default_email_change_from_name');
 }
 add_filter('init','wp_change_default_email');
 
@@ -28,8 +28,8 @@ function wp_change_default_email_change_from_email($email_old) {
 }
 
 function wp_change_default_email_change_from_name($fromname) {
-        global $wcdeOptions;
-        if ( !empty($wcdeOptions['fromname'])) {
+    global $wcdeOptions;
+    if ( !empty($wcdeOptions['fromname'])) {
 	 	return $wcdeOptions['fromname'];
 	} else {
 		return $fromname;
@@ -57,6 +57,13 @@ function wp_change_default_email_settings_link($action_links,$plugin_file){
 	return $action_links;
 }
 add_filter('plugin_action_links','wp_change_default_email_settings_link',10,2);
+
+//Load Translations
+function wcde_load_lang()
+{
+    load_plugin_textdomain('wp-change-default-email', false, basename( dirname( __FILE__ ) ) . '/lang' );
+}
+add_action('init', 'wcde_load_lang');
 
 //If this is admin load the settings page in Admin
 if(is_admin()) require_once('wp_change_default_email_admin.php');
--- wp_change_default_email_admin.php
+++ wp_change_default_email_admin.php
@@ -1,4 +1,13 @@
 <?php
+/*
+Plugin Name: WP Change Default Email
+Plugin URI: http://www.techeach.com/wordpress-plugins/
+Description: WP Change Default Email allows you to change the default from email and name.
+Version: 0.2
+License: GPLv2
+Author: Vijay Sharma
+Author URI: http://www.techeach.com/
+*/
 
 function wp_change_default_email_admin() {
 	add_options_page('WP Change Default Email Options', 'WP Change Default Email','manage_options', 
@@ -14,21 +23,21 @@ function wp_change_default_email_page() {
 
 	    update_option("wp_change_default_email_options",$wcdeOptions);
 	    if(!is_email($wcdeOptions["from"])){
-		echo '<div id="message" class="updated error fade"><p><strong>' . __("The field \"From\" must be a valid email address!") . '</strong></p></div>';
+			echo '<div id="message" class="updated error fade"><p><strong>' . __("The field \"From\" must be a valid email address!") . '</strong></p></div>';
 	    } else{
-		echo '<div id="message" class="updated fade"><p><strong>' . __("Options saved.") . '</strong></p></div>';
+			echo '<div id="message" class="updated fade"><p><strong>' . __("Options saved.", 'wp-change-default-email') . '</strong></p></div>';
    	    }
 	}
-      ?>
+?>
 <div class="wrap">
 	
-<h2> WP Change Default Email </h2>
+<h2><?php _e("WP Change Default Email", 'wp-change-default-email'); ?></h2>
 
 <form action="" method="post" enctype="multipart/form-data" name="wp_change_default_email_form">
 <table class="form-table">
 	<tr valign="top">
 		<th scope="row">
-			<?php _e('From'); ?>
+			<?php _e('From','wp-change-default-email'); ?>
 		</th>
 		<td>
 			<label>
@@ -38,7 +47,7 @@ function wp_change_default_email_page() {
 	</tr>
 	<tr valign="top">
 		<th scope="row">
-			<?php _e('From Name'); ?>
+			<?php _e('From Name', 'wp-change-default-email'); ?>
 		</th>
 		<td>
 			<label>
@@ -50,21 +59,21 @@ function wp_change_default_email_page() {
 
 <p class="submit">
 <input type="hidden" name="wp_change_default_email_update" value="update" />
-<input type="submit" class="button-primary" name="Submit" value="<?php _e('Save Changes'); ?>" />
+<input type="submit" class="button-primary" name="Submit" value="<?php _e('Save Changes', 'wp-change-default-email'); ?>" />
 </p>
 
 </form>
 
 <br />
-<h2>Donate</h2>
+<h2><?php __('Donate','wp-change-default-email'); ?></h2>
 <p>
-If you find my work useful and you want to encourage the development of more free resources, you can do it by donating.
+<?php __('If you find my work useful and you want to encourage the development of more free resources, you can do it by donating.','wp-change-default-email'); ?>
 </p>
 <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-<input type="hidden" name="cmd" value="_s-xclick">
-<input type="hidden" name="hosted_button_id" value="JKWDG57E8SCMA">
-<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+	<input type="hidden" name="cmd" value="_s-xclick">
+	<input type="hidden" name="hosted_button_id" value="JKWDG57E8SCMA">
+	<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+	<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
 </form>
 
 <br />
